e2cd15e6f2cdf07c12830acf51d67518f0717c73
Sanidhya Kumar Tiwari <sanidhyakumartiwari@dyn-192-173-14-169.celerityinternet.com>
2026-07-17 16:16:55 +0530

    Fix RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,32 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
+
 export const metadata: Metadata = {
   metadataBase: new URL("https://personal-website-nu-mocha-22.vercel.app"),
 
